c69000963bec1f704668ab3d83c1c1f9bef23e0b
rainanirmal <rainanirmal01@gmail.com>
2026-07-27 22:50:01 +0530

    feat(String) : add brute force code for rotate string

---
+++ Strings/rotate_string.java
@@ -0,0 +1,41 @@
+package Strings;
+
+import java.util.*;
+
+public class rotate_string {
+
+    public static boolean rotate(String s, String goal) {
+
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        for(int i = 0 ; i < s.length(); i ++) {
+            
+            String rotated = s.substring(i) + s.substring(0, i);
+
+            if (rotated.equals(goal)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String goal = sc.next();
+        System.out.println(rotate(s, goal));
+
+        sc.close();
+        
+    }
+}
+
+// output
+// abcde
+// cdeab
+// true
