987539e89c3ae5614d64275b4c1377df9d619785
rainanirmal <rainanirmal01@gmail.com>
2026-07-27 22:53:01 +0530

    feat(Strings) : add optimal solution for rotate string

--- Strings/rotate_string.java
+++ Strings/rotate_string.java
@@ -4,22 +4,34 @@ import java.util.*;
 
 public class rotate_string {
 
+    // public static boolean rotate(String s, String goal) {
+
+    //     if (s.length() != goal.length()) {
+    //         return false;
+    //     }
+
+    //     for(int i = 0 ; i < s.length(); i ++) {
+            
+    //         String rotated = s.substring(i) + s.substring(0, i);
+
+    //         if (rotated.equals(goal)) {
+    //             return true;
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
     public static boolean rotate(String s, String goal) {
 
         if (s.length() != goal.length()) {
             return false;
         }
 
-        for(int i = 0 ; i < s.length(); i ++) {
-            
-            String rotated = s.substring(i) + s.substring(0, i);
-
-            if (rotated.equals(goal)) {
-                return true;
-            }
-        }
-
-        return false;
+        String doubled = s + s;
+        
+        return doubled.contains(goal);
+        
     }
     
     public static void main(String[] args) {
