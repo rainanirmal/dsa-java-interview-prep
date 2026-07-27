1710fb50ea6850a66701e51fb6646af8779d8f08
rainanirmal <rainanirmal01@gmail.com>
2026-07-27 23:33:11 +0530

    feat(Strings) : add optimal solution for valid anagram

--- Strings/valid_anagram.java
+++ Strings/valid_anagram.java
@@ -4,21 +4,46 @@ import java.util.*;
 
 public class valid_anagram {
 
+    // public static boolean anagram_check(String s, String t) {
+
+    //     if(s.length() != t.length()) { 
+    //         return false;
+    //     }
+
+    //     char[] sArray = s.toCharArray();
+    //     char[] tArray = t.toCharArray();
+
+    //     Arrays.sort(sArray);
+    //     Arrays.sort(tArray);
+
+    //     return Arrays.equals(sArray, tArray);
+    // }
+    
     public static boolean anagram_check(String s, String t) {
 
-        if(s.length() != t.length()) { 
+        if (s.length() != t.length()) {
             return false;
         }
 
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
+        int[] hash = new int[26];
 
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
+        for(char c: s.toCharArray()) {
+            hash[c - 'a'] ++;
+        }
 
-        return Arrays.equals(sArray, tArray);
+        for(char c: t.toCharArray()) {
+            hash[c - 'a'] --;
+        }
+
+        for(int i = 0 ; i < s.length() ; i ++) {
+            if(hash[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
-    
+
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
