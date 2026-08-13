8c57c32795866a21f087de37674a6570ed29d340
rainanirmal <rainanirmal01@gmail.com>
2026-08-13 19:50:59 +0530

    feat(Arrays): add input of an array case 2

--- Arrays/array_input.java
+++ Arrays/array_input.java
@@ -10,15 +10,32 @@ public class array_input {
 
         // case 1: fixed size
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        // int n = sc.nextInt();
+        // int[] arr = new int[n];
 
-        for(int i = 0 ; i < n ; i ++) {
-            arr[i] = sc.nextInt();
+        // for(int i = 0 ; i < n ; i ++) {
+        //     arr[i] = sc.nextInt();
+        // }
+
+        // for (int i : arr) {
+        //     System.out.print(i + " ");
+        // }
+
+        // case 2: dynamic size
+
+        String input = sc.nextLine();
+        String arr1[] = input.split(",");
+
+        ArrayList<Integer> int_input = new ArrayList<>();
+
+        for (String token : arr1) {
+            int_input.add(Integer.parseInt(token));
         }
 
-        for (int i : arr) {
-            System.out.print(i + " ");
+        // Java collections work with objects, not primitive types.
+        // That's why when you're working with ArrayList, List, HashSet, HashMap, etc., you commonly see Integer.
+        for (Integer integer : int_input) {
+            System.out.print(integer+ " ");
         }
 
         sc.close();
