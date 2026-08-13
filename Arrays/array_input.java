8d44f281477c9e61fa7b9ea3d2157b1553d53273
rainanirmal <rainanirmal01@gmail.com>
2026-08-13 19:59:11 +0530

    feat(Arrays): add input of an array case 3

--- Arrays/array_input.java
+++ Arrays/array_input.java
@@ -23,17 +23,35 @@ public class array_input {
 
         // case 2: dynamic size
 
+        // String input = sc.nextLine();
+        // String arr1[] = input.split(",");
+
+        // ArrayList<Integer> int_input = new ArrayList<>();
+
+        // for (String token : arr1) {
+        //     int_input.add(Integer.parseInt(token));
+        // }
+
+        // // Java collections work with objects, not primitive types.
+        // // That's why when you're working with ArrayList, List, HashSet, HashMap, etc., you commonly see Integer.
+
+        // for (Integer integer : int_input) {
+        //     System.out.print(integer+ " ");
+        // }
+
         String input = sc.nextLine();
-        String arr1[] = input.split(",");
 
-        ArrayList<Integer> int_input = new ArrayList<>();
-
-        for (String token : arr1) {
-            int_input.add(Integer.parseInt(token));
+        if(input.startsWith("[") && input.endsWith("]")) {
+            input = input.substring(1, input.length() - 1);
         }
 
-        // Java collections work with objects, not primitive types.
-        // That's why when you're working with ArrayList, List, HashSet, HashMap, etc., you commonly see Integer.
+        String[] arr = input.split(",");
+        ArrayList<Integer> int_input = new ArrayList<>();
+
+        for (String tokens : arr) {
+            int_input.add(Integer.parseInt(tokens));
+        }
+
         for (Integer integer : int_input) {
             System.out.print(integer+ " ");
         }
