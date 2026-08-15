fdf75f311a2cb56876763ee71336f594781cd265
rainanirmal <rainanirmal01@gmail.com>
2026-08-15 13:07:40 +0530

    feat(Arrays): add largest element in array using linear search

---
+++ Arrays/largest_element_in_array.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+import java.util.*;
+
+public class largest_element_in_array {
+
+    public static int larget_element(ArrayList<Integer> arr) {
+
+        int max = 0;
+
+        for (Integer i : arr) {
+            if(i > max) {
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+    // Input: [1,2,3,4,5,89]
+    // Output: 89
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        input = input.substring(1, input.length()-1);
+
+        String[] string_arr = input.split(",");
+
+        ArrayList<Integer> int_arr = new ArrayList<>();
+
+        for (String elements : string_arr) {
+            int_arr.add(Integer.parseInt(elements.trim()));
+        }
+
+        int ans = larget_element(int_arr);
+
+        System.out.println(ans);
+
+        sc.close();
+    }
+}
