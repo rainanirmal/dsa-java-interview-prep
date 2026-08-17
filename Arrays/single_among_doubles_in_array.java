95bf085f84a4e262b3bcf6a606e6426cad0a3da3
rainanirmal <rainanirmal01@gmail.com>
2026-08-17 11:20:27 +0530

    feat(Arrays): add single among doubles in array solution using XOR operation

--- Arrays/single_among_doubles_in_array.java
+++ Arrays/single_among_doubles_in_array.java
@@ -6,33 +6,33 @@ import java.util.*;
 
 public class single_among_doubles_in_array {
 
-    public static int single_among_doubles(int[] arr) {
+    // public static int single_among_doubles(int[] arr) {
 
-        if (arr.length == 1) {
-            return arr[0];
-        }
+    //     if (arr.length == 1) {
+    //         return arr[0];
+    //     }
 
-        for(int i = 0; i < arr.length; i ++) {
-            if (i == 0) {
-                if (arr[i] != arr[i+1]) {
-                    return arr[i];
-                }
-            }
-            else if (i == arr.length - 1) {
-                if (arr[i] != arr[i - 1]) {
-                    return arr[i];
-                }
-            }
-            else {
-                if (arr[i] != arr[i-1] && arr[i] != arr[i+1]) {
-                    return arr[i];
-                }
-            }
-        }
+    //     for(int i = 0; i < arr.length; i ++) {
+    //         if (i == 0) {
+    //             if (arr[i] != arr[i+1]) {
+    //                 return arr[i];
+    //             }
+    //         }
+    //         else if (i == arr.length - 1) {
+    //             if (arr[i] != arr[i - 1]) {
+    //                 return arr[i];
+    //             }
+    //         }
+    //         else {
+    //             if (arr[i] != arr[i-1] && arr[i] != arr[i+1]) {
+    //                 return arr[i];
+    //             }
+    //         }
+    //     }
 
-        return -1;
+    //     return -1;
 
-    }
+    // }
 
     // Enter size of array: 
     // 3
@@ -41,6 +41,35 @@ public class single_among_doubles_in_array {
     // 1
     // 3
     // Single among doubles in array: 
+    // 3
+
+    // xor
+    public static int single_among_doubles(int[] arr) {
+
+        int ans = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            ans = ans ^ arr[i];
+        }
+
+        return ans;
+
+        // 0 ^ 0 = 0
+        // 1 ^ 0 = 1
+        // 1 ^ 1 = 0
+    }
+
+    // Enter size of array:
+    // 7
+    // Enter elements of array:
+    // 1
+    // 1
+    // 2
+    // 2
+    // 3
+    // 4
+    // 4
+    // Single among doubles in array:
     // 3
     
     public static void main(String[] args) {
